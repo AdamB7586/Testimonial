d6f80b285af9c8ca20dc3339c1b0975299a3a629
Adam Binnersley <abinnersley@gmail.com>
2020-10-21 16:27:31 +0100

    Update testing environment

--- tests/TestimonialTest.php
+++ tests/TestimonialTest.php
@@ -7,12 +7,13 @@ use PHPUnit\Framework\TestCase;
 
 class TestimonialTest extends TestCase
 {
-    
+    protected $db;
     protected $testimonial;
     
     public function setUp(): void
     {
-        $this->testimonial = new Testimonial(new Database('127.0.0.1', 'root', '', 'test_db'));
+        $this->db = new Database($GLOBALS['DB_HOST'], $GLOBALS['DB_USER'], $GLOBALS['DB_PASSWD'], $GLOBALS['DB_DBNAME']);
+        $this->testimonial = new Testimonial($this->db);
     }
     
     public function tearDown(): void
